a75f4a208f1d55ea82f71fe911cea87f2b29e2ed
Chukaflu16 <areyesg04@hotmail.com>
2022-08-30 23:56:12 -0500

    Initial commit

---
+++ Lab12/lab12.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorio 1.2</title>
+</head>
+<body>
+    <?php
+        $n1 = 1;
+        $n2 = 2;
+        $suma = $n1 + $n2;
+        echo "Suma = " . $suma. "<br>";
+        echo  "$n1 + $n2";
+    ?>
+</body>
+</html>
---
+++ Lab17/lab17.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laboratorio 1.7</title>
+</head>
+<body>
+    <?php
+        $x = 24;
+        $pi = 3.1416;
+        $animal = "conejo";
+        $saludo = "Hola caracola";
+        echo $x , "<br>" , $pi, "<br>", $animal, "<br>", $saludo; 
+    ?>
+</body>
+</html>
